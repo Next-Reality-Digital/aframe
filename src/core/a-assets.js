4c1e8c8eb6c59b24726a554170a5b4342d354e45
Angel Cabrera <acabrera@nextreality.digital>
2025-02-03 13:18:35 -0400

    feat: attach to r158

--- src/core/a-assets.js
+++ src/core/a-assets.js
@@ -1,15 +1,15 @@
-var ANode = require('./a-node');
-var registerElement = require('./a-register-element').registerElement;
+var ANode = require("./a-node");
+var registerElement = require("./a-register-element").registerElement;
 
 // TODO we only use a-assets as a container for NAF templates, it can be removed once we move those out
-module.exports = registerElement('a-assets', {
+module.exports = registerElement("a-assets", {
   prototype: Object.create(ANode.prototype, {
     attachedCallback: {
       value: function () {
         // HACK There are some strange intermittent startup race conditions caused by making this sync.
         // Having a delay here (as there was before with content loading) seems to eliminate them. 0 does not.
-        setTimeout(() => this.load(), 500)
-      }
-    }
-  })
+        setTimeout(() => this.load(), 500);
+      },
+    },
+  }),
 });
