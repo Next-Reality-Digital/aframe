8b24062eaf8f8d4f73bd43429bd56656d2a07832
Angel Cabrera <acabrera@nextreality.digital>
2025-03-12 16:02:44 +0000

    feat: clean unused systems and components

--- src/systems/index.js
+++ src/systems/index.js
@@ -1,9 +1,1 @@
-require('./geometry.js');
-require('./material.js');
-require('./obb-collider.js');
-require('./tracked-controls.js');
 require('./webxr.js');
-require('./gltf-model.js');
-require('./renderer.js');
-require('./tracked-controls-webvr');
-require('./tracked-controls-webxr');
